1a4b15f26e8a26a5e04b9fa66aecbc6bd1264c07
ivan11velazquez <ivan1994.253@gmail.com>
2016-05-14 13:02:01 -0400

    ahora funciona con ManagerDB

--- java/com/par/paronline/modelo/ABMCliente.java
+++ java/com/par/paronline/modelo/ABMCliente.java
@@ -5,56 +5,52 @@
  */
 package com.par.paronline.modelo;
 
-import com.par.paronline.utils.Conexion;
-import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import javax.naming.NamingException;
 /**
  *
  * @author User
  */
-
 public class ABMCliente {
 
     public static ArrayList<Usuario> listar() throws SQLException, Exception{
-        String sql = "select id_usuario,nombre,apellido,email, nombre_usuario from usuarios;";
-        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
-        Connection conn = Conexion.getConexion();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
-        
-        while(rs.next()){
+        ManagerDB db = new ManagerDB();
+        String sql = "select id_usuario,nombre,apellido,email from usuarios;";
+        ArrayList<Usuario> usuarios = new ArrayList();
+        db.consultar(sql);
+        while(db.getResult().next()){
             Usuario u = new Usuario();
-            u.setId_usuario(rs.getInt("id_usuario"));
-            u.setNombre(rs.getString("nombre"));
-            u.setApellido(rs.getString("apellido"));
-            u.setEmail(rs.getString("email"));
-            u.setNombre_usuario(rs.getString("nombre_usuario"));
+            u.setId_usuario(db.getResult().getInt("id_usuario"));
+            u.setNombre(db.getResult().getString("nombre"));
+            u.setApellido(db.getResult().getString("apellido"));
+            u.setDireccion(db.getResult().getString("direccion"));
+            u.setEmail(db.getResult().getString("email"));
+            u.setRol(db.getResult().getString("rol"));
+            
             usuarios.add(u);
         }
-        stmt.close();
-        Conexion.closeConexion(conn);
+        db.cerrarConexion();
         return usuarios;
     }
-    //el metodo buscar usuario debe ser buscado mediante otro parametro por ejemplo el nombre de usuario
-    public static Usuario buscarUsuario(String id_usuario) throws ClassNotFoundException, SQLException, Exception{
+    
+    public static Usuario buscarUsuario(String email) throws ClassNotFoundException, SQLException, Exception{
         Usuario u = null;
-        Connection conn = Conexion.getConexion();
-        PreparedStatement stmt = conn.prepareStatement("select * from usuarios where id_usuario = ?");
-        stmt.setString(1,id_usuario);
-        ResultSet rs = stmt.executeQuery();
-        if(rs.next()){
+        ManagerDB db = new ManagerDB();
+        PreparedStatement stmt = db.getPrepareStatement("select*from usuarios where email =?");
+        stmt.setString(1,email);
+        db.iduquery(stmt);
+        if(db.getResult().next()){
             u = new Usuario();
-            u.setId_usuario(rs.getInt("id_usuario"));
-            u.setNombre(rs.getString("nombre"));
-            u.setApellido(rs.getString("apellido"));
-            u.setEmail(rs.getString("email"));
-            u.setDireccion(rs.getString("direccion"));
-            u.setContrasenha(rs.getString("contrasenha"));
+            u.setId_usuario(db.getResult().getInt("id_usuario"));
+            u.setNombre(db.getResult().getString("nombre"));
+            u.setApellido(db.getResult().getString("apellido"));
+            u.setEmail(db.getResult().getString("email"));
+            u.setDireccion(db.getResult().getString("direccion"));
+            u.setContrasenha(db.getResult().getString("contrasenha"));
             
             return u;
         }
@@ -62,49 +58,43 @@ public class ABMCliente {
     }
     
     public static void agregar(Usuario u){
-        //la funcion no se encarga de agregar el id_usuario ya que este es una sequencia en la base de datos
         try{
-            Connection conn = Conexion.getConexion();
-            conn.setAutoCommit(true);
-            PreparedStatement stmt = conn.prepareStatement("insert into Usuarios (nombre, apellido, contrasenha, direccion, email, rol, nombre_usuario)"
-                    + " values (?,?,?,?,?,?,?)");
-            stmt.setString(1,u.getNombre());
-            stmt.setString(2,u.getApellido());
-            stmt.setString(3,u.getContrasenha());
-            stmt.setString(4,u.getDireccion());
-            stmt.setString(5,u.getEmail());
-            stmt.setString(6,"U");
-            stmt.setString(7, u.getNombre_usuario());//aqui seteamos el nombre de usuario
-            stmt.executeUpdate();
-            stmt.close();
-            Conexion.closeConexion(conn);
-        }catch(Exception e){System.out.println("Error al guardar usuario:"+e);}
-    }
-    //no hace falta que la funcion reciba el mail ni el id.
-    public static void actualizar (Usuario u, int id, String mail) throws SQLException, Exception{
-        Connection conn = Conexion.getConexion();
-        conn.setAutoCommit(true);
-        PreparedStatement stmt = conn.prepareStatement("update usuarios set"
-                + "nombre = ?,apellido = ?,contrasenha = ? ,direccion = ? ,email = ? where id_usuario = ?");
+        ManagerDB db = new ManagerDB();
+        String sql = "insert into usuarios(nombre,apellido,contrasenha,direccion,email,rol,nombre_usuario) values (?,?,?,?,?,?,?)";
+        db.getPrepareStatement(sql);
+        PreparedStatement stmt = db.getPrepareStatement(sql);
         stmt.setString(1,u.getNombre());
         stmt.setString(2,u.getApellido());
         stmt.setString(3,u.getContrasenha());
         stmt.setString(4,u.getDireccion());
         stmt.setString(5,u.getEmail());
-        stmt.setInt(6,u.getId_usuario());
+        stmt.setString(6,u.getRol());
+        stmt.setString(7,u.getNombre_usuario());
         
-        
-        stmt.executeUpdate();
-        stmt.close();
-        Conexion.closeConexion(conn);
+        db.iduquery(stmt);
+        db.cerrarConexion();
+        }catch(Exception e){System.out.println("Error al guardar usuario:"+e);}
     }
     
-    public boolean existeUsuario(String id_usuario) {
+    public static void actualizar (Usuario u, String mail) throws SQLException, Exception{
+        ManagerDB db = new ManagerDB();
+        PreparedStatement stmt = db.getPrepareStatement("update usuarios set"
+                + "nombre = ?,apellido = ?,contrasenha = ? ,direccion = ? where email = ?");
+        stmt.setString(1,u.getNombre());
+        stmt.setString(2,u.getApellido());
+        stmt.setString(3,u.getContrasenha());
+        stmt.setString(4,u.getDireccion());
+        db.iduquery(stmt);
+        db.cerrarConexion();
+    }
+    
+    public boolean existeUsuario(String email) {
         boolean existe = false;
-        String sql = "select * from usuario where id_usuario ="+id_usuario+";";
+        String sql = "select * from usuario where email = ?;";
         try{
-        Connection conn = Conexion.getConexion();
-        Statement stmt = conn.createStatement();
+        ManagerDB db = new ManagerDB();
+        PreparedStatement stmt = db.getPrepareStatement(sql);
+        stmt.setString(1,email);
         ResultSet rs = stmt.executeQuery(sql);
         if(rs.next()){
             existe = true;
@@ -115,64 +105,53 @@ public class ABMCliente {
         return existe;
     }
     
-    public static void borrarUsuario(String id_usuario) throws SQLException, Exception{
-        Connection conn = Conexion.getConexion();
-        conn.setAutoCommit(true);
-        PreparedStatement stmt = conn.prepareStatement("delete from usuarios where id_usuario =?");
-        stmt.setString(1,id_usuario);
+    public static void borrarUsuario(String email) throws SQLException, Exception{
+        ManagerDB db = new ManagerDB();
+        String sql = "delete from usuarios where email =?";
+        PreparedStatement stmt = db.getPrepareStatement(sql);
+        stmt.setString(1,email);
         stmt.executeUpdate();
         stmt.close();
-        Conexion.closeConexion(conn);
+        db.cerrarConexion();
     } 
     
-    public static String verificarEmail(String email){
-        String existe = null;
+    public static boolean verificarEmail(String email){
+        String sql = "select email from usuarios where email = ?";
+      
         try{
-        Connection conn = Conexion.getConexion();
-        PreparedStatement stmt = conn.prepareStatement("select email from usuarios where email = ?");
+        ManagerDB db = new ManagerDB();
+        PreparedStatement stmt = db.getPrepareStatement(sql);
         stmt.setString(1,email);
-        ResultSet rs = stmt.executeQuery();
-        if(rs.next()){
-            existe = "La direccion de correo electronico ya se encuentra en uso!";
-            return existe;
-        } else return "";
-        }catch(Exception e){System.out.print("Error:"+e);}
+        db.iduquery(stmt);
+        if(db.getResult().next()){
+            return true;
+        }
+        }catch(Exception e){System.out.print("Error:"+e);}      
         
-        return existe;        
-    
+        return false;
     }
-    //debe verificar el nombre de usuario
-    public static String verificarId(String id_usuario){
-        String existe = null;
-        try{
-        Connection conn = Conexion.getConexion();
-        PreparedStatement stmt = conn.prepareStatement("select id_usuario from usuarios where id_usuario=?");
-        stmt.setString(1,id_usuario);
-        ResultSet rs = stmt.executeQuery();
-        if(rs.next()){
-            existe = "El nombre de usuario elegido ya se encuentra en uso!";
-            return existe;
-        } else return "";
-        }catch(Exception e){System.out.print("Error:"+e);}
+            
+    
         
-        return existe;        
-    
-    }
-    //debe comprobar el nombre de usuario y contrasenha     
-    public static boolean comprobarCredenciales(String id_usuario, String contrasenha) throws ClassNotFoundException, NamingException, Exception{
-            Connection conn = Conexion.getConexion();
-            conn.setAutoCommit(true);
-            PreparedStatement stmt = conn.prepareStatement("select id_usuario, contrasenha from usuarios where id_usuario = ? and contrasenha = ?");
-            stmt.setString(1,id_usuario);
-            stmt.setString(2,contrasenha);
-            
-            ResultSet rs = stmt.executeQuery();
-            
-            if(rs.next()){
-                return true;
-            }
-            
-            return false;
+    public static Usuario comprobarLogin(String email, String contrasenha) throws ClassNotFoundException, NamingException, Exception{
+        ManagerDB db = new ManagerDB();
+        Usuario u = null;
+        ArrayList<String> args = new ArrayList();
+        args.add(email);
+        args.add(contrasenha);
+           
+        db.consultar("select * from usuarios where email = ? and contrasenha = ?", args);
+
+        if(db.getResult().next()){
+            u = new Usuario();
+            u.setId_usuario(db.getResult().getInt("id_usuario"));
+            u.setNombre(db.getResult().getString("nombre"));
+            u.setApellido(db.getResult().getString("apellido"));
+            u.setEmail(db.getResult().getString("email"));
+            u.setRol(db.getResult().getString("rol"));
+            return u;
+        }
+        return u;
     }
         
            
